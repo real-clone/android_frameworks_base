f46ffc3cd8ac4c83f4a93b931a198de2a5412c2f
firebird11 <hbgassel@gmail.com>
2016-03-15 09:45:26 +0100

    Revert "NavigationBarView: Set buttonWithTagVisibility for Search"
    This reverts commit 02f4e17b58dd28b97ead9af2473fe6fcbf09f748.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -473,7 +473,6 @@ public class NavigationBarView extends LinearLayout {
         setButtonWithTagVisibility(NavbarEditor.NAVBAR_BACK, !disableBack);
         setButtonWithTagVisibility(NavbarEditor.NAVBAR_HOME, !disableHome);
         setButtonWithTagVisibility(NavbarEditor.NAVBAR_RECENT, !disableRecent);
-        setButtonWithTagVisibility(NavbarEditor.NAVBAR_SEARCH, !disableSearch);
     }
 
     private boolean inLockTask() {
